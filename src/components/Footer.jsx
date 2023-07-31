5ae67c3d822ca3cff4e8f4c16ad07ada65e87125
Rutika Nitnaware <130966188+Nitnawarerutika@users.noreply.github.com>
2023-07-31 13:43:35 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
     return    (    
     <pre className="Footer">
         <span>
-        {/* Made with 🧡    |   &copy; 2022 <a href="https://github.com/eshabaweja">Esha Baweja</a> */}
+      
         Made with 🧡    |   &copy; 2023 Rutika Nitnaware
         </span>
     </pre>
